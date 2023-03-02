4ccb023364ffe3399b704a2c8239d682f03313bc
Caishangqi <Caiziii@126.com>
2023-03-02 16:04:23 +0800

    barriot implement version

--- Lab3/exercise-3/PERL_BLAST.pl
+++ Lab3/exercise-3/PERL_BLAST.pl
@@ -1,88 +1,38 @@
 ﻿#!/usr/bin/perl
-# use strict;
 use warnings FATAL => 'deprecated';
 use strict;
 
-{package record;
-sub new {
-    my $class = shift;
-    my $self = {
-        _stringID         => shift,
-        _locationInString => shift,
-    };
-    # 输出用户信息
-    print "stringID: $self->{_stringID}\n";
-    print "locationInString: $self->{_locationInString}\n";
-    bless $self, $class;
-    return $self;
-}
-sub setStringID {
-    my ($self, $stringID) = @_;
-    $self->{_stringID} = $stringID if defined($stringID);
-    return $self->{_stringID};
-}
-
-sub getStringID {
-    my ($self) = @_;
-    return $self->{_stringID};
-}
-
-sub setLocationInString {
-    my ($self, $location) = @_;
-    $self->{_locationInString} = $location if defined($location);
-    return $self->{_locationInStringD};
-}
-
-sub getLocationInString {
-    my ($self) = @_;
-    return $self->{_locationInString};
-}
-1;}
-
-sub findKmerWithFirstLocation {
+sub findKmer {
     my @param_list = @_;
     my $dna = $param_list[0];
-    # print "string is $dna\n";
     chomp $dna;
+
     my $k = 4;
-    chomp $k;
-    my %kmer = (); # This initializes the hash called kmer.
-    my %fixed_kmer = ();
+
+    my %kmer = ();
     my $i = 1;
+
     while (length($dna) >= $k)
     {
         $dna =~ m/(.{$k})/;
-        #print "$1, $i \n";
         if (!defined $kmer{$1})
         {
-            #defined is a function that returns true
-            # if a value has already been assigned to
-            # $kmer{$1}, otherwise it returns false. 
-            # the ! character is the negation, so
-            # if $kmer{$1} has no value, then it will
-            # be assigned the value of $i, the position
-            # where the k-mer is first found.
-            $kmer{$1} = $i;
+            $kmer{$1} = [ $i ];
+        }
+        else
+        {
+            push(@{$kmer{$1}}, $i)
         }
         $i++;
         $dna = substr($dna, 1, length($dna) - 1);
     }
 
-    foreach my $kmerkey (keys(%kmer))
-    {
+    # foreach my $kmerkey (sort keys(%kmer)) {
+    #     my $occrs = join(', ', @{$kmer{$kmerkey}});
+    #     print "The occurrences of string $kmerkey are in positions $occrs \n";
+    # }
 
-        $kmerkey =~ s/\s+$//;
-        # remove not 4-kmer element
-        if (length($kmerkey) == 4)
-        {
-            $fixed_kmer{$kmerkey} = $kmer{$kmerkey};
-            my $l = length($kmerkey);
-            #print("length = $l $kmerkey\n");
-        }
-
-    }
-
-    return (%fixed_kmer)
+    return %kmer;
 }
 
 # it should return the length of match
@@ -149,6 +99,28 @@ sub scanString {
     return ($total_match)
 }
 
+sub hashTableToString {
+    my %parameter = @_;
+    foreach my $key (keys %parameter)
+    {
+        # ARRAY(0xffff) ARRAY(0xffff)
+        # OR
+        # ARRAY(0xffff)
+
+        # print "@{@{$hash_table{$key}}[0]} \n";
+        my @res;
+        for (@{$parameter{$key}})
+        {
+            my @arr = @{$_};
+            my $str = join(", ", @{$arr[1]});
+            push(@res, "[$arr[0], $str]");
+            # push(@res, "[$arr[0], $arr[1]]");
+        }
+        print "$key " . join(", ", @res) . "\n";
+        # print "@{$hash_table{$key}}\n"
+    }
+
+}
 open _FileQ, "Q.txt";
 open _FileS, "S.txt";
 
@@ -176,145 +148,72 @@ while (my $line = <_FileS>)
     push(@data_s, $line)
 }
 
+# hash table build
 my %hash_table;
 
 for my $i (0 .. $#data_s)
 {
-    my %kmers_with_location = findKmerWithFirstLocation($data_s[$i]);
+    # key: [...int]
+    my %kmers = findKmer($data_s[$i]);
 
-    while ((my $key, my $value) = each(%kmers_with_location))
+    while ((my $key, my $value) = each(%kmers))
     {
-        if (exists($hash_table{$key}))
-        {
-            push(@{$hash_table{$key}}, [$i, $value]);
-        }
-        else
+        if (!defined $hash_table{$key})
         {
             $hash_table{$key} = [ [ $i, $value ] ];
         }
+        else
+        {
+            push(@{$hash_table{$key}}, [ $i, $value ]);
+        }
     }
 }
 
-my $i = 0;
+hashTableToString(%hash_table);
 
-foreach my $key (keys %hash_table)
-{
-    # ARRAY(0xffff) ARRAY(0xffff)
-    # OR
-    # ARRAY(0xffff)
-
-    # print "@{@{$hash_table{$key}}[0]} \n";
-    my @res;
-    for (@{$hash_table{$key}})
-    {
-        my @arr = @{$_};
-        push(@res, "[$arr[0], $arr[1]]");
-    }
-    print "$key " . join(", ", @res) . "\n";
-    # print "@{$hash_table{$key}}\n"
-}
-
-# for (my $i = 0; $i < scalar(@all_S_data); $i++) {
-#     my %kmers_with_location = findKmerWithFirstLocation($all_S_data[$i]); # ther, 33
-#
-#     foreach my $kmer_key (keys(%kmers_with_location)) {
-#         print("DEBUG: $kmer_key + $kmers_with_location{$kmer_key}\n");
-#         # if the hash table already have the kmer
-#         if (exists($hash_table{$kmer_key})) {
-#             print("this kmer $kmer_key exist in hashtable so added to the kmer key~\n");
-#
-#             my @cache_list = $hash_table{$kmer_key};
-#             my $index = length(@cache_list);
-#
-#             my $record = new record($i, $kmers_with_location{$kmer_key});
-#
-#             my $index_after = length(@cache_list);
-#             $cache_list[$index] = $record;
-#             #$hash_table{$kmer_key}->[$index] = $record;
-#
-#             print("==========================================================\n");
-#             print("The multiple value in hash is: $hash_table{$kmer_key}\n");
-#             print("right now hash table length is: BEFORE: $index, AFTER: $index_after\n");
-#             print("==========================================================\n");
-#
-#         }
-#         else {
-#             my @cache_array = ();
-#             my $record = new record($i, $kmers_with_location{$kmer_key});
-#             $cache_array[0] = $record; #[record]
-#             print("DEBUG: current cache_array =  $cache_array[0]\n");
-#
-#             $hash_table{$kmer_key} = @cache_array;
-#
-#             print("DEBUG: hashtable value =  $hash_table{$kmer_key}\n");
-#             print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-#             @cache_array = $hash_table{$kmer_key};
-#             my $index = length(@cache_array);
-#             print("right now hash table length is: $index\n");
-#             print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-#         }
-#
+# 模板 
+# foreach my $key (keys %hash_table) {
+#     # ARRAY(0xffff) ARRAY(0xffff)
+#     # OR
+#     # ARRAY(0xffff)
+# 
+#     # print "@{@{$hash_table{$key}}[0]} \n";
+#     my @res;
+#     for (@{$hash_table{$key}}) {
+#         my @arr = @{$_};
+#         push(@res, "[$arr[0], $arr[1]]");
 #     }
-#
+#     print "$key " . join(", ", @res) . "\n";
+#     # print "@{$hash_table{$key}}\n"
 # }
 
-# print the hashtable
+#hashTableToString(%hash_table);
 
-# key: [record1,record2],
-# key: []
+# hash table build complete!
 
-# foreach my $kmer_key (keys(%hash_table))
-# {
-#     print("$kmer_key => ");
-#
-#     my @cache_list = \$hash_table{$kmer_key};
-#     # my $baibaicha = {};
-#     # print(ref($hash_table));
-#     # my $aaaa->isa($hash_table{$kmer_key});
-#     # print("CACHED LIST: $aaaa");
-#     my $record = $cache_list[0];
-#     print($record->record::getStringID);
-#     # my @cache_list = $hash_table{$kmer_key};
-#     #
-#     # for (my $i = 0; $i < length(@cache_list); $i++) {
-#     #     my $record = $hash_table{$kmer_key}->[$i];
-#     #     print($record->record::getStringID . " " . $record->record::getLocationInString);
-#     # }
-#
-#     # for my $records (\@cache_list) {
-#     #     if ($records != "") {
-#     #         print($records->record::getStringID, $records->record::getLocationInString)
-#     #     }
-#     #
-#     # }
-#
-#     print("\n")
-#
-# }
+my $all_S_in_line = "";
+# all S in one line
+foreach my $data_s (@data_s)
+{
+    $data_s =~ chomp $data_s;
+    $all_S_in_line = $all_S_in_line . $data_s
+}
+
+# print("TOTAL S = $all_S_in_line\n");
+# print("\n");
 
 
 # 每当 S 中的 4-mer 被确定为在 Q 中时，提取该 4-mer 在 Q 中第一次出现的位置。
-
-# my @location_Q;
-# my @location_S;
-# foreach my $kmer_key (keys(%hash_table_S_kmers)) {
-#     if (exists($hash_table_Q_kmers{$kmer_key})) {
-# 
-#         @location_Q = ($hash_table_Q_kmers{$kmer_key}, $kmer_key);
-#         @location_S = ($hash_table_S_kmers{$kmer_key}, $kmer_key);
-# 
-#     }
+# my %kmers_with_location_IN_Q = findKmerWithFirstLocation($query_Q);
+# while ((my $key, my $value) = each(%kmers_with_location_IN_Q))
+# {
+#     #print("($key,$value)\n")
 # }
-# 
-# print("@location_Q\n");
-# print("===========\n");
-# print("@location_S\n");
 
 # Then put the characters of Q and S in arrays (as we did in needleman.pl) so
 # that you can examine individual characters.
 
 #split up the line into individual characters
-
 
 # Then scan left from the k-mer in Q and in S, as long
 # as you find matching characters. Repeat to the right. Let L denote the length of the whole match
---
+++ Lab3/exercise-3/kmer-test.pl
@@ -0,0 +1,46 @@
+﻿#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+sub findKmerWithFirstLocation {
+    my @param_list = @_;
+    my $dna = $param_list[0];
+    # print "string is $dna\n";
+    chomp $dna;
+    my $k = 4;
+    chomp $k;
+    my %kmer = (); # This initializes the hash called kmer.
+    my %fixed_kmer = ();
+    my $i = 1;
+    while (length($dna) >= $k) {
+        $dna =~ m/(.{$k})/;
+        #print "$1, $i \n";
+        if (!defined $kmer{$1}) {
+            #defined is a function that returns true
+            # if a value has already been assigned to
+            # $kmer{$1}, otherwise it returns false. 
+            # the ! character is the negation, so
+            # if $kmer{$1} has no value, then it will
+            # be assigned the value of $i, the position
+            # where the k-mer is first found.
+            $kmer{$1} = $i;
+        }
+        $i++;
+        $dna = substr($dna, 1, length($dna) - 1);
+    }
+
+    foreach my $kmerkey (keys(%kmer)) {
+
+        $kmerkey =~ s/\s+$//;
+        # remove not 4-kmer element
+        if (length($kmerkey) == 4) {
+            $fixed_kmer{$kmerkey} = $kmer{$kmerkey};
+            my $l = length($kmerkey);
+            print("($kmerkey,$fixed_kmer{$kmerkey})\n");
+        }
+
+    }
+
+    return (%fixed_kmer)
+}
+
+findKmerWithFirstLocation("rlgadvlashgrlvqyrgevqamlgqsteelrvrlashlrklrkrllrvlashqkrlavy")
