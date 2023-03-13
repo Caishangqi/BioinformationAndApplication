ce79de67433330ca5124367492b8af73a48d8175
Caishangqi <Caiziii@126.com>
2023-03-13 11:40:37 +0800

    exercise 2

---
+++ Lab4/exercise-2/nbjtree.py
@@ -0,0 +1,47 @@
+from Bio import SeqIO, Phylo
+from Bio.Align import MultipleSeqAlignment, Alignment
+from Bio.Seq import Seq
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
+from Bio.Phylo import write
+from Bio.SeqRecord import SeqRecord
+
+# # Define the filenames for the fasta files
+filenames = ['German_Neanderthal.fasta', 'Russian_Neanderthal.fasta', 'European_Human.fasta',
+             'Mountain_Gorilla_Rwanda.fasta',
+             'Chimp_Troglodytes.fasta', 'Puti_Orangutan.fasta', 'Jari_Orangutan.fasta', 'Western_Lowland_Gorilla.fasta',
+             'Eastern_Lowland_Gorilla.fasta', 'Chimp_Schweinfurthii.fasta', 'Chimp_Vellerosus.fasta',
+             'Chimp_Verus.fasta']
+
+# Create a list to store the sequences
+sequences = {}
+
+# Read in the fasta files and add the sequences to the list
+for filename in filenames:
+    with open(filename) as file:
+        for record in SeqIO.parse(file, 'fasta'):
+            sequences[record.id] = str(record.seq)
+
+# Determine the shortest sequence length
+min_length = min([len(seq) for seq in sequences.values()])
+
+msa = MultipleSeqAlignment([])
+# Truncate all sequences to the shortest length
+for name, seq in sequences.items():
+    sequences[name] = seq[:min_length]
+    print(name + ":\n")
+    print(sequences[name] + "\n")
+    msa.append(SeqRecord(id=name, seq=sequences[name]))
+print(msa)
+
+calculator = DistanceCalculator('identity')
+
+# distance_matrix = calculator.get_distance(sequences.values())
+distance_matrix = calculator.get_distance(msa)
+
+# Create an neighbor-joining tree from the distance matrix
+constructor = DistanceTreeConstructor()
+tree = constructor.nj(distance_matrix)
+
+Phylo.draw(tree)
+# Write the tree to a csv file
+write(tree, 'ultrametric_tree_nbj.csv', 'phyloxml')
